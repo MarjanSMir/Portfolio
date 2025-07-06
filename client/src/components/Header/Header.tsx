e84e41369288f6f5ea4799fb8d67bab6662d9570
Marjansmir <marjansmirmohammad@outlook.com>
2025-07-06 17:00:10 +0330

    CV Button

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,16 +1,23 @@
-import Nav from "./../Menu/Nav"
-const Header: React.FC = ()=> {
-
+import Nav from "./../Menu/Nav";
+const Header: React.FC = () => {
   return (
     <article className="w-full h-screen  bg-[url(/client/src/assets/Home-bg.webp)] bg-cover font-serif home-bg flex justify-center flex-column items-center">
       <Nav />
-      <div className="text-gray-800 w-[70rem] h-[25rem] backdrop-blur-xs flex justify-center items-center flex-col box">
+      <div className="text-gray-800 w-[70rem] h-[25rem] backdrop-blur-xs flex justify-center items-center flex-col box mt-20">
         <h1 className="text-5xl">Hi! I'm Marjan</h1>
-        <h2 className="text-3xl leading-20">UI/UX Designer and Web Developer</h2>
+        <h2 className="text-3xl leading-20">
+          UI/UX Designer and Web Developer
+        </h2>
+        <button
+          type="button"
+          className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 me-2 mb-2 mt-10"
+        >
+          <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+          <a href="" className="text-l">My Resume</a>
+        </button>
       </div>
     </article>
-
   );
-}
+};
 
 export default Header;
--- client/src/pages/About/About.tsx
+++ client/src/pages/About/About.tsx
@@ -7,14 +7,17 @@ function About() {
       <div className="box-2 w-10/12 h-9/12 backdrop-blur-md opacity-80 shadow-md fixed  mt-25 flex justify-center flex-column items-center leading-8">
         <h1 className="text-3xl mr-36">About Me</h1>
         <p className="w-4/6 text-2xl ">
-          I’m a web developer with a Bachelor’s degree in <strong>Computer Engineering</strong> from West Tehran University. My journey into web development began
-          with learning web design, which quickly grew into a deep passion for
-          building modern, responsive websites. <br /> Specialize in <strong>front-end
-          and back-end technologies</strong> including HTML, CSS, JavaScript, React.js,
-          Node.js, Tailwind, and Bootstrap. I enjoy crafting clean, efficient
-          code and continuously learning new tools and frameworks to enhance my
-          skills. <br /> I’m committed to creating digital experiences that are
-          both visually appealing and user-friendly.
+          I’m a web developer with a Bachelor’s degree in{" "}
+          <strong>Computer Engineering</strong> from West Tehran University. My
+          journey into web development began with learning web design, which
+          quickly grew into a deep passion for building modern, responsive
+          websites. <br /> Specialize in{" "}
+          <strong>front-end and back-end technologies</strong> including HTML,
+          CSS, JavaScript, React.js, Node.js, Tailwind, and Bootstrap. I enjoy
+          crafting clean, efficient code and continuously learning new tools and
+          frameworks to enhance my skills. <br /> I’m committed to creating
+          digital experiences that are both visually appealing and
+          user-friendly.
         </p>
       </div>
     </article>
